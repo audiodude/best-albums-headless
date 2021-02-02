160577b032e7f5da6afff3d19f476fac81c1a44f
Travis Briggs <audiodude@gmail.com>
2021-02-01 19:39:36 -0800

    Add albums controller, and home page link to albums if user is logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'albums/index'
+  get 'albums/new'
+  get 'albums/create'
+  get 'albums/show'
+  get 'albums/edit'
+  get 'albums/update'
+  get 'albums/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
