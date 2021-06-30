9aaede7ca5f6b84ed7c3f6338ccb4820af5750c2
Travis Briggs <audiodude@gmail.com>
2021-06-29 23:04:38 -0700

    ActiveStorage attachment for album cover

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,18 @@
 class Album < ApplicationRecord
+  has_one_attached :cover
+  before_validation :update_slug
+
+  def update_slug
+    self.slug = "#{artist} #{title}".parameterize[0..40]
+  end
+
+  def update_cover!
+    if !self.cover_url.empty?
+      client = HTTPClient.new(default_header: {'User-Agent' => 'BestAlbumsBot 0.1.0/Audiodude <audiodude@gmail.com>'})
+      file = StringIO.new(client.get_content(cover_url))
+      self.cover.attach(io: file, filename: "cover#{cover_url[-4]}")
+      return true
+    end
+    return false
+  end
 end
