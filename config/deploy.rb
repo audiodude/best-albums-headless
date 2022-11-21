531fa99f75aec1905b5d25efeb07ab437a528bff
Travis Briggs <audiodude@gmail.com>
2022-11-20 18:31:46 -0800

    Add capistrano task for running remote rake tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,10 @@ set :keep_releases, 3
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+namespace :rake do  
+  desc "Run a task on a remote server."  
+  # run like: cap staging rake:invoke task=a_certain_task  
+  task :invoke do  
+    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")  
+  end  
+end
